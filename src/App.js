190a730388e2e0e19103686e91d47319294adfdd
SURYA CHAUDHARY <suryachaudhary.216@gmail.com>
2024-06-05 20:37:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import ImageSlider from "./ImageSlider";
 const App = () => {
   const slides = [
-    { url: "http://localhost:3000/image-1.jpg", title: "Audi-R8" },
-    { url: "http://localhost:3000/image-2.jpg", title: "Dodge Challener" },
-    { url: "http://localhost:3000/image-3.jpg", title: "Ford Mustang" },
-    { url: "http://localhost:3000/image-4.jpg", title: "McLaren" },
-    { url: "http://localhost:3000/image-5.jpg", title: "Porshe" },
-    { url: "http://localhost:3000/image-6.jpg", title: "Toyoto Supra" },
+    { url: "image-1.jpg", title: "Audi-R8" },
+    { url: "image-2.jpg", title: "Dodge Challener" },
+    { url: "image-3.jpg", title: "Ford Mustang" },
+    { url: "image-4.jpg", title: "McLaren" },
+    { url: "image-5.jpg", title: "Porshe" },
+    { url: "image-6.jpg", title: "Toyoto Supra" },
   ];
   const containerStyles = {
     width: "1200px",
